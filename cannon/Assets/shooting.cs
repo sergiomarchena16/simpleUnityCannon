675e567ea28150fe33fddcfccdfbb8f0f19d022d
Sergio A. Marchena G <mar16387@uvg.edu.gt>
2020-09-24 16:32:33 -0600

    cannon
    cannon

---
+++ cannon/Assets/shooting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public Transform shootPos;
+    public GameObject sphere;
+    public float shotPower;
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(sphere, shootPos.position, shootPos.rotation).GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * shotPower);
+        }
+    }
+}
